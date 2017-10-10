ad93bc985ce4ad87c248bf31e0cd7c5f0f9273dd
ISAACPC\isaactij <isaactij@utexas.edu>
2017-10-10 10:50:27 -0500

    Revert "test now use junit5, add validRowsCols func and tests"
    This reverts commit 59fd199ad0e7e1a1a6fc6035576635dbb54b898a.

---
+++ src/tests.java
@@ -0,0 +1,22 @@
+
+public class tests {
+
+	public static void main(String[] args) {
+		print_board_tests();
+
+	}
+	
+	public static void print_board_tests(){
+		String expected = "0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0";
+		int[][] input = {{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0}};
+		String output = sudoku_main.convert_board_to_string(input);
+		if(expected.equals(output)){
+			System.out.println("convert_board_to_string worked");
+		}else{
+			System.out.println("convert_board_to_string failed");
+			System.out.println("Expected: \n" + expected);
+			System.out.println("Output: \n" + output);
+		}
+	}
+
+}
