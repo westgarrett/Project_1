86c61d19d01b090cca2bb2c9258444063d37ac51
ISAACPC\isaactij <isaactij@utexas.edu>
2017-10-09 22:36:22 -0500

    changed all methods and variable names to lower case letters

--- src/sudoku_main.java
+++ src/sudoku_main.java
@@ -1,38 +1,38 @@
 
-public class Sudoku_Main {
+public class sudoku_main {
 	public static void main(String[] args) {
 		//Main board
 		int[][] board = new int[9][9];
-		print_Board(board);
+		print_board(board);
 
 	}
 	
-	public static void print_Board(int[][] board){
+	public static void print_board(int[][] board){
 		//Calls method Convert_Board_To_String to make the board into a printable string
-		String string_Board = convert_Board_To_String(board);
-		System.out.print(string_Board);
+		String string_board = convert_board_to_string(board);
+		System.out.print(string_board);
 	}
 	
-	public static String convert_Board_To_String(int[][] board){
+	public static String convert_board_to_string(int[][] board){
 		//Start with empty string
-		String string_Board = "";
+		String string_board = "";
 		//For every row
 		for(int i = 0; i < board.length; i++){
 			//For every column
 			for(int j = 0; j < board[i].length; j++){
 				//Adds integer from board at row and column specified
-				string_Board += board[i][j];
+				string_board += board[i][j];
 				//If it is not at the last integer of the row then adds space|space
 				if(j < board[i].length - 1){
-					string_Board += " | ";
+					string_board += " | ";
 				}
 			}
 			//If it is not the last row then it adds the new line character
 			if(i < board.length - 1){
-				string_Board += "\n";
+				string_board += "\n";
 			}			
 		}
-		return string_Board;
+		return string_board;
 	}
 
 }
--- src/tests.java
+++ src/tests.java
@@ -1,19 +1,19 @@
 
-public class Tests {
+public class tests {
 
 	public static void main(String[] args) {
-		print_Board_Tests();
+		print_board_tests();
 
 	}
 	
-	public static void print_Board_Tests(){
+	public static void print_board_tests(){
 		String expected = "0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0\n0 | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0";
 		int[][] input = {{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0},{0,0,0,0,0,0,0,0,0}};
-		String output = Sudoku_Main.convert_Board_To_String(input);
+		String output = sudoku_main.convert_board_to_string(input);
 		if(expected.equals(output)){
-			System.out.println("convert_Board_To_String worked");
+			System.out.println("convert_board_to_string worked");
 		}else{
-			System.out.println("convert_Board_To_String failed");
+			System.out.println("convert_board_to_string failed");
 			System.out.println("Expected: \n" + expected);
 			System.out.println("Output: \n" + output);
 		}
